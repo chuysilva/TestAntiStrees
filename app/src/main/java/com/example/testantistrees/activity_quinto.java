16b80366421af6694c0d942994d83ee25e251610
ChuyPc <albertosilvarincon@gmail.com>
2022-10-30 17:18:27 -0600

    Avance 30/10/2022

--- app/src/main/java/com/example/testantistrees/activity_quinto.java
+++ app/src/main/java/com/example/testantistrees/activity_quinto.java
@@ -2,7 +2,9 @@ package com.example.testantistrees;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_quinto extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class activity_quinto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quinto);
+    }
+
+    public void Siguiente(View view){
+        Intent sig = new Intent(this, activity_sexto.class);
+        startActivity(sig);
     }
 }
--- app/src/main/java/com/example/testantistrees/activity_sexto.java
+++ app/src/main/java/com/example/testantistrees/activity_sexto.java
@@ -2,7 +2,9 @@ package com.example.testantistrees;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_sexto extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class activity_sexto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sexto);
+    }
+
+    public void Siguiente(View view){
+        Intent sig = new Intent(this, activity_septima.class);
+        startActivity(sig);
     }
 }
